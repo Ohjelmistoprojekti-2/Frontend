8f7de79f7e1813305b12cf6da27772dcd14be1b2
el1sa <elisa.tuovila@myy.haaga-helia.fi>
2022-03-01 11:06:23 +0200

    job.json Resulst.js modified

--- components/Results.js
+++ components/Results.js
@@ -1,27 +1,20 @@
 //TÄNNE LISTATAAN HAKUTULOKSET TYÖPAIKOISTA
 // NÄKYMÄSSÄ TÄLLÄ HETKELLÄ KAIKKI JOBS.JSONISSA OLEVAT TYÖPAIKKATIEDOT
 // EI YHDISTETTY HOME KOPMPONENTTIIN
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import Data from "./jobs.json";
 
 export default function Results({ route, navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Juu</Text>
+      <Text>Results: </Text>
       {Data &&
         Data.map((item) => {
           return (
             <View style={styles.info} key={item.id}>
+              <Text>{item.header}</Text>
               <Text>{item.company}</Text>
-              {item &&
-                item.content.map((data) => {
-                  return (
-                    <View key={item.id}>
-                      <Text>{data.header}</Text>
-                      <Text>{data.url}</Text>
-                    </View>
-                  );
-                })}
+              <Text>{item.url}</Text>
             </View>
           );
         })}
@@ -31,11 +24,9 @@ export default function Results({ route, navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
     marginTop: 50,
   },
   info: {
-    margin: 24,
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
