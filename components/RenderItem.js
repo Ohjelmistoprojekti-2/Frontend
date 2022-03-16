4892bb602a96710a5b2d8e422f0b202ba533d913
Elisa <elisa.tuovila@gmail.com>
2022-03-16 12:44:16 +0200

    jobs.json muokkaus #2

--- components/RenderItem.js
+++ components/RenderItem.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Linking, TouchableOpacity  } from "react-native";
+import { View, Text, Linking, TouchableOpacity } from "react-native";
 import * as Colorthemes from "./styles";
 
 const colorthemes = Colorthemes.colorthemes;
@@ -25,20 +25,20 @@ export default function RenderItem({ item, yestags, notags, locations }) {
   //// - tekstistä ei löydy yhtään notags-tagia
 
 
-    // jos item on selvinnyt seulasta
-    return (
-      <View style={[colorthemes.resultStyles.items, {display: naytetaan ? 'block' : 'none' }]}>
-        <Text style={colorthemes.resultStyles.job}>{item._values.header}</Text>
-        <Text style={colorthemes.resultStyles.coname}>
-          {item._values.company}
-        </Text>
-        <TouchableOpacity
-          styles={colorthemes.resultStyles.button}
-          onPress={() => Linking.openURL(`${item._values.url}`)}
-        >
-          <Text style={colorthemes.resultStyles.buttonText}>See more</Text>
-        </TouchableOpacity>
-      </View>
-    );
+  // jos item on selvinnyt seulasta
+  return (
+    <View style={[colorthemes.resultStyles.items, { display: naytetaan ? 'block' : 'none' }]}>
+      <Text style={colorthemes.resultStyles.job}>{item._values.header}</Text>
+      <Text style={colorthemes.resultStyles.coname}>
+        {item._values.company}
+      </Text>
+      <TouchableOpacity
+        styles={colorthemes.resultStyles.button}
+        onPress={() => Linking.openURL(`${item._values.url}`)}
+      >
+        <Text style={colorthemes.resultStyles.buttonText}>See more</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
 }
