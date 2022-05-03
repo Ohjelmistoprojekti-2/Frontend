c8356cacacfe634a784726a836037a9a9fc11ae8
el1sa <elisa.tuovila@myy.haaga-helia.fi>
2022-05-03 10:34:11 +0300

    modified filterFunctions.js and testjobs.js #13

--- components/__tests__/filterFunctions.test.js
+++ components/__tests__/filterFunctions.test.js
@@ -28,85 +28,92 @@ const notags = [
 const locations = [
     'Helsinki',
     'Espoo',
-    'Oulu'
+    'Oulu',
+    'Jyväskylä'
 ]
 
-// rivien 6-36 tapaukset testaavat, että funktio filtteröi ja palauttaa oikeanpituiset arrayt
-describe('function returns an array of two objects', () => {
+// rivien 35-71 tapaukset testaavat, että funktio filtteröi ja palauttaa oikeanpituiset arrayt
+
+// palauttaa arrayn, jossa valitut yritykset
+describe('coJobs function returns an array of two objects', () => {
     const expected = coJobs(jobs, userOptions);
-    it('length of the array is two', () => {
+    test('length of the array is two', () => {
         expect(expected.length).toBe(2);
     });
 
 })
 
-describe('function returns with three objects', () => {
+// palauttaa arrayn, jossa käyttäjän valitsemat hakusanat
+describe('yesTags function returns with three objects', () => {
     const expected = yesTags(jobs, yestags);
-    it('length of the array is three', () => {
+    test('length of the array is three', () => {
         expect(expected.length).toBe(3);
     });
 
 })
 
-describe('function returns an array of three objects', () => {
+// palauttaa arrayn, jossa ei ole käyttäjän kirjaamia sanoja
+describe('noTags function returns an array of two objects', () => {
     const expected = noTags(jobs, notags);
-    it('length of the array is three', () => {
-        expect(expected.length).toBe(3);
+    test('length of the array is two', () => {
+        expect(expected.length).toBe(2);
     });
 
 })
 
+// palauttaa arrayn, jossa on käyttäjän kirjaamat paikkakunnat
 describe('jobLocations function returns an array of three objects', () => {
     const expected = jobLocations(jobs, locations);
-    it('length of the array is four', () => {
+    test('length of the array is four', () => {
         expect(expected.length).toBe(4);
     });
 
 })
 
-// // rivien 39-83 tapaukset testaavat, että funktiot palauttavat arrayn, jotkä sisältävät täsmälleen oikeanlaiset objektit
-// describe('function returns an array of jobs that contains only company names of Reaktor and Visma', () => {
-//     it('the received array matches with the expected one', () => {
-//         expect(filterFunctions.coJobs).toEqual(
-//             expect.arrayContaining([
-//                 expect.objectContaining(filterFunctions.testjobs[0]),
-//                 expect.objectContaining(filterFunctions.testjobs[1])
-//             ]));
-//     });
+// rivien 73-114 tapaukset testaavat, että funktiot palauttavat arrayn, jotkä sisältävät täsmälleen oikeanlaiset objektit
+describe('function returns an array of jobs that contains only company names of Reaktor and Visma', () => {
+    test('the received array matches with the expected one', () => {
+        expect(coJobs(jobs, userOptions)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining(jobs[0]),
+                expect.objectContaining(jobs[1])
+            ]));
+    });
 
-// })
+})
 
-// describe('function returns an array of jobs that contains only companies that include yestags Frontend and DevOps', () => {
-//     it('the received array matches with the expected one', () => {
-//         expect(filterFunctions.yesTags).toEqual(
-//             expect.arrayContaining([
-//                 expect.objectContaining(filterFunctions.testjobs[0])
-//             ]));
-//     });
+describe('function returns an array of jobs that contains only companies that include yestags Frontend and DevOps', () => {
+    test('the received array matches with the expected one', () => {
+        expect(yesTags(jobs, yestags)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining(jobs[0]),
+                expect.objectContaining(jobs[1]),
+                expect.objectContaining(jobs[4])
+            ]));
+    });
 
-// })
+})
 
-// describe('function returns an array of jobs that do not include notags Azure and Python', () => {
-//     it('the received array matches with the expected one', () => {
-//         expect(filterFunctions.noTags).toEqual(
-//             expect.arrayContaining([
-//                 expect.objectContaining(filterFunctions.testjobs[0]),
-//                 expect.objectContaining(filterFunctions.testjobs[1]),
-//                 expect.objectContaining(filterFunctions.testjobs[4])
-//             ]));
-//     });
+describe('function returns an array of jobs that do not include notags Azure and Python', () => {
+    test('the received array matches with the expected one', () => {
+        expect(noTags(jobs, notags)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining(jobs[0]),
+                expect.objectContaining(jobs[1])
+            ]));
+    });
 
-// })
+})
 
-// describe('function returns an array of jobs that have Helsinki, Espoo or Oulu as locations', () => {
-//     it('the received array matches with the expected one', () => {
-//         expect(filterFunctions.jobLocations).toEqual(
-//             expect.arrayContaining([
-//                 expect.objectContaining(filterFunctions.testjobs[1]),
-//                 expect.objectContaining(filterFunctions.testjobs[2]),
-//                 expect.objectContaining(filterFunctions.testjobs[3]),
-//                 expect.objectContaining(filterFunctions.testjobs[4])
-//             ]));
-//     });
+describe('function returns an array of jobs that have Helsinki, Espoo or Oulu as locations', () => {
+    test('the received array matches with the expected one', () => {
+        expect(jobLocations(jobs, locations)).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining(jobs[1]),
+                expect.objectContaining(jobs[2]),
+                expect.objectContaining(jobs[3]),
+                expect.objectContaining(jobs[4])
+            ]));
+    });
 
-// })
+})
