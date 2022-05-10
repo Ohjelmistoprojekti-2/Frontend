4f7d62ce9e6cf4e21c6ed57091992cc0aac41e47
jazmc <jasmin.lumme@gmail.com>
2022-05-10 09:47:07 +0300

    more seconds to test.....

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -21,7 +21,7 @@ it("loads jobs from backend api", () => {
     .then((jobs) => {
       cy.log("**navigating to job listing page**");
       cy.contains("Job results").click();
-      cy.wait(8000); // odotetaan 8 sek koska github actions workflow on niin hidas että failaa muuten
+      cy.wait(10000); // odotetaan 10 sek koska github actions workflow on niin hidas että failaa muuten
       cy.get("[data-testid=job]").should("have.length", jobs.length);
     });
 });
